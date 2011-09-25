a8a639dbb542955137354bba262d68cf0be99ca6
Adrian Perez <aperez@igalia.com>
2011-09-26 01:14:36 +0200

    Added Spanish translation

---
+++ src/l10n/es.ts
@@ -0,0 +1,133 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>AboutView</name>
+    <message>
+        <location filename="../qml/AboutView.qml" line="6"/>
+        <source>This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.&lt;br /&gt;&lt;br /&gt;This package is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.&lt;br /&gt;&lt;br /&gt;You should have received a copy of the GNU General Public License along with this program. If not, see &lt;a href=&quot;http://www.gnu.org/licenses&quot;&gt;http://www.gnu.org/licenses&lt;/a&gt;&lt;br /&gt;&lt;br /&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutView.qml" line="101"/>
+        <source>This application uses &lt;a href=&quot;http://meneame.net/&quot;&gt;Meneame&lt;/a&gt; but it is not affiliated nor certified by them.</source>
+        <translation>Esta aplicación usa &lt;a href=&quot;http://meneame.net/&quot;&gt;Menéame&lt;/a&gt; pero no está afiliado ni certificado por ellos..</translation>
+    </message>
+</context>
+<context>
+    <name>CommentsDelegate</name>
+    <message>
+        <location filename="../qml/CommentsDelegate.qml" line="60"/>
+        <source>#%1 | karma: %2 | By %3</source>
+        <translation>#%1 | karma: %2 | Por %3</translation>
+    </message>
+</context>
+<context>
+    <name>CommentsHeader</name>
+    <message>
+        <location filename="../qml/CommentsHeader.qml" line="27"/>
+        <source>Comments for %1</source>
+        <translation>Comentarios para %1</translation>
+    </message>
+</context>
+<context>
+    <name>CommentsView</name>
+    <message>
+        <location filename="../qml/CommentsView.qml" line="13"/>
+        <source>Open original news</source>
+        <translation>Ver noticia original</translation>
+    </message>
+    <message>
+        <location filename="../qml/CommentsView.qml" line="179"/>
+        <source>%1 comments</source>
+        <translation>%1 comentarios</translation>
+    </message>
+</context>
+<context>
+    <name>MainView</name>
+    <message>
+        <location filename="../qml/MainView.qml" line="70"/>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainView.qml" line="95"/>
+        <source>Category</source>
+        <translation>Categoría</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainView.qml" line="206"/>
+        <source>Error while accessing Meneame</source>
+        <translation>Error al acceder a Menéame</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainView.qml" line="210"/>
+        <source>Try again</source>
+        <translation>Volver a intentar</translation>
+    </message>
+</context>
+<context>
+    <name>NewsDelegate</name>
+    <message>
+        <location filename="../qml/NewsDelegate.qml" line="77"/>
+        <source>shakes</source>
+        <translation>meneos</translation>
+    </message>
+    <message>
+        <location filename="../qml/NewsDelegate.qml" line="103"/>
+        <source>cool</source>
+        <translation>chachi</translation>
+    </message>
+    <message>
+        <location filename="../qml/NewsDelegate.qml" line="105"/>
+        <source>vote</source>
+        <translation>vota</translation>
+    </message>
+    <message>
+        <location filename="../qml/NewsDelegate.qml" line="109"/>
+        <source>error</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/NewsDelegate.qml" line="209"/>
+        <source>%1 comments</source>
+        <translation>%1 comentarios</translation>
+    </message>
+    <message>
+        <location filename="../qml/NewsDelegate.qml" line="219"/>
+        <source>By %1</source>
+        <translation>Por %1</translation>
+    </message>
+</context>
+<context>
+    <name>RefreshHeader</name>
+    <message>
+        <location filename="../qml/RefreshHeader.qml" line="70"/>
+        <source>Tap to update</source>
+        <translation>Pulsa para actualizar</translation>
+    </message>
+    <message>
+        <location filename="../qml/RefreshHeader.qml" line="70"/>
+        <source>Updating</source>
+        <translation>Actualizando</translation>
+    </message>
+    <message>
+        <location filename="../qml/RefreshHeader.qml" line="78"/>
+        <source>Last update: %1</source>
+        <translation>Última actualización: %1</translation>
+    </message>
+</context>
+<context>
+    <name>l10n-dummy</name>
+    <message>
+        <location filename="../qml/l10n-dummy.qml" line="2"/>
+        <source>Frontpage</source>
+        <translation>Portada</translation>
+    </message>
+    <message>
+        <location filename="../qml/l10n-dummy.qml" line="3"/>
+        <source>Pending</source>
+        <translation>Pendientes</translation>
+    </message>
+</context>
+</TS>
